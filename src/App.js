f7f3f01427583eb1ce2716d2209d42f82b0a0d86
bao <tranthanhbao0309@gmail.com>
2022-06-06 22:15:18 +0700

    Completed feature

--- src/App.js
+++ src/App.js
@@ -1,36 +1,77 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Control from "./components/Control";
 function App() {
-  var myImage = document.querySelector("img");
+  //URL for API call
+  // const myURL = "http://0854-113-185-73-33.ngrok.io";
+  const [myURL, setMyURL] = useState("");
+  const [errorMsg, setErrorMsg] = useState(false);
+  const myImage = document.getElementById("image-camera");
+  const imagesFunc = async () => {
+    try {
+      await fetch(`${myURL}/cam0`).then(function (response) {
+        // console.log(response);
+        return response.blob();
+      })
+      .then(function (myBlob) {
+        if (myBlob) {
+          var objectURL = URL.createObjectURgitL(myBlob);
+          myImage.src = objectURL;
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      myImage.src = "../public/image/waiting-icon-gif-23.jpg";
+    }
+  };
+  const handleSubmitURL = () => {
+    console.log(document.getElementById("url").value);
+    setMyURL(document.getElementById("url").value);
+    document.getElementById("url").value = "";
+  };
+
   useEffect(() => {
-    let interval = setInterval(() => {
-      try {
-        fetch("http://1d19-113-185-75-100.ngrok.io/cam0")
-          .then(function (response) {
-            return response.blob();
-          })
-          .then(function (myBlob) {
-            var objectURL = URL.createObjectURL(myBlob);
-            myImage.src = objectURL;
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }, 100);
-    return () => clearInterval(interval);
-  }, []);
+    try {
+      fetch(`${myURL}/items/1`).then(function (response) {
+        if (response.status === 200) {
+          setErrorMsg(false);
+          let myInterval = setInterval(() => {
+            imagesFunc();
+          }, 500);
+          return () => clearInterval(myInterval);
+        } else setErrorMsg(true);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [myURL]);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Home page</h1>
-        <img src=""></img>
-      </header>
-      <body>
-        <Control />
-      </body>
+      <div className="header">
+        <h1>Control Your Robot </h1>
+        <img src="/image/icons8-chatbot-64.png" alt="logo" className="logo" />
+      </div>
+      <div className="form-field">
+        <input id="url" type="text" className="form-input" />
+        <label for="url" className="form-label">
+          URL
+        </label>
+        <button onClick={handleSubmitURL}>Submit</button>
+      </div>
+      <div className="error">
+        {errorMsg ? <p>Your URL is not correct</p> : <div></div>}
+      </div>
+
+      <div className="body">
+        <img
+          id="image-camera"
+          src="/image/waiting-icon-gif-23.jpg"
+          alt="data"
+          className="camera"
+        ></img>
+        <Control url={myURL} />
+      </div>
     </div>
   );
 }
-
 export default App;
--- src/components/Control.js
+++ src/components/Control.js
@@ -1,11 +1,13 @@
-const Control = (props) => {
-  var forward = 0;
-  var back = 0;
-  var left = 0;
-  var right = 0;
-  var rotateLeft = 0;
-  var rotateRight = 0;
+import "./Control.css";
 
+const Control = (props) => {
+  let forward = 0;
+  let back = 0;
+  let left = 0;
+  let right = 0;
+  let rotateLeft = 0;
+  let rotateRight = 0;
+  const step = 20;
   const requestForward = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -36,136 +38,178 @@ const Control = (props) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify([0, 0, -1]),
   };
+
+  // function to call API
+  function moving(move, request) {
+    if (move >= step) {
+      try {
+        fetch(`${props.url}/data/omnidrive`, request)
+          .then((response) => response.json())
+          .then((data) => this.setState({ postId: data.id }));
+        // fetch(`${props.url}/items/1`).then(function (response) {
+          // console.log(response);
+        // });
+      } catch (error) {}
+      console.log("call API done!");
+    }
+  }
+
   const update = () => {
     const gamepads = navigator.getGamepads();
 
     if (gamepads[0]) {
       const gamepadState = {
-        id: gamepads[0].id,
+        // id: gamepads[0].id,
 
-        axes: [
-          gamepads[0].axes[0].toFixed(2),
-          gamepads[0].axes[1].toFixed(2),
-          gamepads[0].axes[2].toFixed(2),
-          gamepads[0].axes[3].toFixed(2),
-        ],
+        // axes: [
+        //   gamepads[0].axes[0].toFixed(2),
+        //   gamepads[0].axes[1].toFixed(2),
+        //   gamepads[0].axes[2].toFixed(2),
+        //   gamepads[0].axes[3].toFixed(2),
+        // ],
 
         buttons: [
-          { button_0: gamepads[0].buttons[0].pressed },
+          // { button_0: gamepads[0].buttons[0].pressed },
           { button_1: gamepads[0].buttons[1].pressed },
           { button_2: gamepads[0].buttons[2].pressed },
-          { button_3: gamepads[0].buttons[3].pressed },
-          { button_4: gamepads[0].buttons[4].pressed },
-          { button_5: gamepads[0].buttons[5].pressed },
-          { button_6: gamepads[0].buttons[6].pressed },
-          { button_7: gamepads[0].buttons[7].pressed },
-          { button_8: gamepads[0].buttons[8].pressed },
-          { button_9: gamepads[0].buttons[9].pressed },
-          { button_10: gamepads[0].buttons[10].pressed },
-          { button_11: gamepads[0].buttons[11].pressed },
+          // { button_3: gamepads[0].buttons[3].pressed },
+          // { button_4: gamepads[0].buttons[4].pressed },
+          // { button_5: gamepads[0].buttons[5].pressed },
+          // { button_6: gamepads[0].buttons[6].pressed },
+          // { button_7: gamepads[0].buttons[7].pressed },
+          // { button_8: gamepads[0].buttons[8].pressed },
+          // { button_9: gamepads[0].buttons[9].pressed },
+          // { button_10: gamepads[0].buttons[10].pressed },
+          // { button_11: gamepads[0].buttons[11].pressed },
           { button_12: gamepads[0].buttons[12].pressed },
           { button_13: gamepads[0].buttons[13].pressed },
           { button_14: gamepads[0].buttons[14].pressed },
           { button_15: gamepads[0].buttons[15].pressed },
         ],
       };
-      const myJSON = JSON.stringify(gamepadState, null, 2);
-      document.getElementById("gamepad-display").innerHTML = myJSON;
+      // const myJSON = JSON.stringify(gamepadState.buttons, null, 2);
+      // document.getElementById("gamepad-display").innerHTML = myJSON;
     }
     if (gamepads[0]) {
       if (gamepads[0].buttons[12].pressed === true) {
-        console.log(forward);
-        back = left = right = 0;
+        document.getElementById("go-forward").style.filter = "grayscale(0)";
+        back = right = left = rotateLeft = rotateRight = 0;
+        // console.log(forward);
         forward++;
-        if (forward >= 20) {
-          fetch(
-            "http://1d19-113-185-75-100.ngrok.io/data/omnidrive",
-            requestForward
-          )
-            .then((response) => response.json())
-            .then((data) => this.setState({ postId: data.id }));
-          forward = forward - 20;
+        if (forward >= step) {
+          moving(forward, requestForward);
+          forward -= step;
         }
-      }
+      } else
+        document.getElementById("go-forward").style.filter = "grayscale(1)";
       if (gamepads[0].buttons[13].pressed === true) {
-        console.log(back);
-        forward = left = right = 0;
+        document.getElementById("go-back").style.filter = "grayscale(0)";
+        forward = right = left = rotateLeft = rotateRight = 0;
         back++;
-        if (back >= 20) {
-          fetch(
-            "http://1d19-113-185-75-100.ngrok.io/data/omnidrive",
-            requestBack
-          )
-            .then((response) => response.json())
-            .then((data) => this.setState({ postId: data.id }));
-          back -= 20;
+        if (back >= step) {
+          moving(back, requestBack);
+          back -= step;
         }
-      }
+      } else document.getElementById("go-back").style.filter = "grayscale(1)";
+
       if (gamepads[0].buttons[14].pressed === true) {
-        console.log(left);
-        back = forward = right = 0;
+        document.getElementById("go-left").style.filter = "grayscale(0)";
+
+        back = right = forward = rotateLeft = rotateRight = 0;
         left++;
-        if (left >= 20) {
-          fetch(
-            "http://1d19-113-185-75-100.ngrok.io/data/omnidrive",
-            requestLeft
-          )
-            .then((response) => response.json())
-            .then((data) => this.setState({ postId: data.id }));
-          left -= 20;
+        if (left >= step) {
+          moving(left, requestLeft);
+          left -= step;
         }
-      }
+      } else document.getElementById("go-left").style.filter = "grayscale(1)";
+
       if (gamepads[0].buttons[15].pressed === true) {
-        back = left = forward = 0;
-        console.log(right);
+        document.getElementById("go-right").style.filter = "grayscale(0)";
+        back = forward = left = rotateLeft = rotateRight = 0;
         right++;
-        if (right >= 20) {
-          fetch(
-            "http://1d19-113-185-75-100.ngrok.io/data/omnidrive",
-            requestRight
-          )
-            .then((response) => response.json())
-            .then((data) => this.setState({ postId: data.id }));
-          right -= 20;
+        if (right >= step) {
+          moving(right, requestRight);
+          right -= step;
         }
-      }
+      } else document.getElementById("go-right").style.filter = "grayscale(1)";
+
       if (gamepads[0].buttons[2].pressed === true) {
+        document.getElementById("go-rotate-left").style.filter = "grayscale(0)";
+
         right = back = left = forward = rotateRight = 0;
-        // console.log(rotateLeft);
         rotateLeft++;
-        if (rotateLeft >= 20) {
-          fetch(
-            "http://1d19-113-185-75-100.ngrok.io/data/omnidrive",
-            requestRotateLeft
-          )
-            .then((response) => response.json())
-            .then((data) => this.setState({ postId: data.id }));
-          rotateLeft -= 20;
+        if (rotateLeft >= step) {
+          moving(rotateLeft, requestRotateLeft);
+          left -= rotateLeft;
         }
-      }
+      } else
+        document.getElementById("go-rotate-left").style.filter = "grayscale(1)";
+
       if (gamepads[0].buttons[1].pressed === true) {
+        document.getElementById("go-rotate-right").style.filter =
+          "grayscale(0)";
         right = back = left = forward = rotateLeft = 0;
-        // console.log(right);
         rotateRight++;
-        if (rotateRight >= 20) {
-          fetch(
-            "http://1d19-113-185-75-100.ngrok.io/data/omnidrive",
-            requestRotateRight
-          )
-            .then((response) => response.json())
-            .then((data) => this.setState({ postId: data.id }));
-          rotateRight -= 20;
+        if (rotateRight >= step) {
+          moving(rotateRight, requestRotateRight);
+          rotateRight -= step;
         }
-      }
+      } else
+        document.getElementById("go-rotate-right").style.filter =
+          "grayscale(1)";
     }
     window.requestAnimationFrame(update);
   };
   window.requestAnimationFrame(update);
 
   return (
-    <div>
+    <div className="control">
       <pre id="gamepad-display"></pre>
       <p id="demo"></p>
+      <div className="control-left">
+        <div className="control-left top">
+          <img
+            src="/image/icons8-thick-arrow-pointing-up-96.png"
+            id="go-forward"
+            className="forward"
+            alt="forward"
+          ></img>
+        </div>
+        <div className="control-left bottom">
+          <img
+            src="/image/icons8-left-96.png"
+            id="go-left"
+            alt="left"
+            className="left"
+          ></img>
+          <img
+            src="/image/icons8-thick-arrow-pointing-down-96.png"
+            id="go-back"
+            alt="back"
+            className="back"
+          ></img>
+          <img
+            src="/image/icons8-right-96.png"
+            id="go-right"
+            alt="right"
+            className="right"
+          ></img>
+        </div>
+      </div>
+      <div className="control-right">
+        <img
+          src="/image/icons8-rotate-left-96.png"
+          id="go-rotate-left"
+          alt="rotate-left"
+          className="rotate-left"
+        ></img>
+        <img
+          src="/image/icons8-rotate-right-96.png"
+          id="go-rotate-right"
+          alt="rotate-right"
+          className="rotate-right"
+        ></img>
+      </div>
     </div>
   );
 };
